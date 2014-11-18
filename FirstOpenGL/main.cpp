cf754b1e38aa7049ac23af7eef7499b106edc3ea
jacobward1898 <jacobward1898@gmail.com>
2014-11-18 10:19:09 -0700

    Initial Commit

---
+++ FirstOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstOpenGL
+//
+//  Created by Jacob Ward on 11/18/14.
+//  Copyright (c) 2014 Jacob Ward. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
